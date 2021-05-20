8a612012c20d848396f57a2fe7b2ed941c93d7a4
Mateusz <mateusz.wilusz.mf14@gmail.com>
2021-05-20 10:25:16 +0200

    Classes to user login and registration

---
+++ shop/src/main/java/com/example/shop/entities/Role.java
@@ -0,0 +1,18 @@
+package com.example.shop.entities;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name="roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private RoleName name;
+}
---
+++ shop/src/main/java/com/example/shop/entities/RoleName.java
@@ -0,0 +1,6 @@
+package com.example.shop.entities;
+
+public enum RoleName {
+    ROLE_USER,
+    ROLE_ADMIN
+}
